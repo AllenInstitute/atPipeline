5dc0b0a4e783886aa276ad70bb71fc6ad90858be
Totte Karlsson <tottek@gmail.com>
2018-10-26 15:24:01 -0700

    Done up to flatfield corrected data

--- atmain.py
+++ atmain.py
@@ -8,15 +8,18 @@ import atutils
 import create_state_tables
 import create_rawdata_render_multi_stacks
 import create_median_files
+import create_flatfield_corrected_data
+import timeit
 
 if __name__ == '__main__':
+    timeStart = timeit.default_timer()
 
     #What data to process??
     dataRootFolder = "F:\\data\\M33"
     ribbons = ["Ribbon0004"]
     sessions = ["Session01",
-                #"Session02",
-                #"Session03"
+                "Session02",
+                "Session03"
                 ]
 
     sessionFolders = []
@@ -31,18 +34,18 @@ if __name__ == '__main__':
     #Render info
     renderProjectName = atutils.getProjectNameFromSessionFolder(sessionFolders[0])
     renderProject  = atutils.RenderProject("ATExplorer", "W10DTMJ03EG6Z.corp.alleninstitute.org", renderProjectName)
-
-##    #Start with the creation of state table files
-##    #create state tables function takes a sessionFolder and start, end section and a dockerContainer as parameters
-##    for sessionFolder in sessionFolders:
-##        create_state_tables.run(sessionFolder, startSection, endSection, dockerContainer)
-
-
-##    #Create Renderstacks (multi) for the raw data
-##    for sessionFolder in sessionFolders:
-##        create_rawdata_render_multi_stacks.run(sessionFolder, startSection, endSection, dockerContainer, renderProject)
-
-    #Calculate median files
     for sessionFolder in sessionFolders:
-        #run(firstsection, lastsection, sessionFolder, dockerContainer, renderProject)
+        #Start with the creation of state table files
+        create_state_tables.run(sessionFolder, startSection, endSection, dockerContainer)
+
+        #Create Renderstacks (multi) for the raw data
+        create_rawdata_render_multi_stacks.run(sessionFolder, startSection, endSection, dockerContainer, renderProject)
+
+        #Calculate median files
         create_median_files.run(startSection, endSection, sessionFolder, dockerContainer, renderProject)
+
+        #Calculate median files
+        create_flatfield_corrected_data.run(startSection, endSection, sessionFolder, dockerContainer, renderProject)
+
+    timeEnd = timeit.default_timer()
+    print("Elapsed time in apply_medians: " + str(timeEnd - timeStart))
--- source/create_flatfield_corrected_data.py
+++ source/create_flatfield_corrected_data.py
@@ -9,33 +9,29 @@ import timeit
 def run(firstsection, lastsection, sessionFolder, dockerContainer, renderProject):
 
     [dataRootFolder, ribbon, session] = atutils.parse_session_folder(sessionFolder)
-    json_prefix = os.path.join("%s"%(dataRootFolder), "logs")
+    #Output directories
+    flatfield_dir    = os.path.join("%s"%dataRootFolder, "processed", "flatfield")
 
     #stacks
     acq_stack        = "ACQ_Session%d"%(session)
     median_stack     = "MED_Session%d"%(session)
     flatfield_stack  = "FF_Session%d"%(session)
 
-    #Output directories
-    flatfield_dir    = os.path.join("%s"%dataRootFolder, "processed", "FlatField")
-
     #Make sure output folder exists
-    if os.path.isdir(logsFolder) == False:
-       os.mkdir(logsFolder)
-
+    if os.path.isdir(flatfield_dir) == False:
+       os.mkdir(flatfield_dir)
 
 	#Create json files and apply median.
     for sectnum in range(firstsection -1, lastsection):
         z = ribbon*100 + sectnum
-        ff_z = ribbon*100 + sectnum
         with open(atutils.flatfieldtemplate) as json_data:
              ff = json.load(json_data)
 
-        flatfieldfile    = os.path.join(json_prefix,"flatfield""_%s_%s_%s_%d.json"%(renderProject.name, ribbon, session, sectnum))
-        atutils.saveflatfieldjson(ff, flatfieldfile, renderProject.host, renderProject.owner, renderProject.name, acq_stack, median_stack, flatfield_stack, atutils.toPosixPath(flatfield_dir, "/mnt"), ff_z, True)
+        flatfield_json    = os.path.join(flatfield_dir, "flatfield""_%s_%s_%s_%d.json"%(renderProject.name, ribbon, session, sectnum))
+        atutils.saveflatfieldjson(ff, flatfield_json, renderProject.host, renderProject.owner, renderProject.name, acq_stack, median_stack, flatfield_stack, atutils.toPosixPath(flatfield_dir, "/mnt"), z, True)
         cmd = "docker exec " + dockerContainer + " python -m rendermodules.intensity_correction.apply_multiplicative_correction"
         cmd = cmd + " --render.port 80"
-        cmd = cmd + " --input_json %s"%(atutils.toPosixPath(flatfieldfile, "/mnt"))
+        cmd = cmd + " --input_json %s"%(atutils.toPosixPath(flatfield_json, "/mnt"))
 
         #Run =============
         print ("Running: " + cmd)
@@ -58,6 +54,6 @@ if __name__ == "__main__":
     renderProject     = atutils.RenderProject("ATExplorer", render_host, renderProjectName)
 
     run(firstsection, lastsection, sessionFolder, dockerContainer, renderProject)
-    timeEnd = timeit.default_timer()
 
-    print("Elapsed time in apply_medians: " + str(timeEnd - timeStart))
+    timeEnd = timeit.default_timer()
+    print("Elapsed time in create_flatfield_corrected_data: " + str(timeEnd - timeStart))
