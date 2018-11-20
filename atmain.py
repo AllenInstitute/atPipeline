4dfe9c997f9fad04377bd8c155c9d5782fabe85f
Totte Karlsson <tottek@gmail.com>
2018-11-20 09:33:09 -0800

    Adding some log messages

--- atmain.py
+++ atmain.py
@@ -26,10 +26,12 @@ if __name__ == '__main__':
     #What data to process??
     prefixPath = ini['PREFIX_PATH']
     dataRootFolder = ini['DATA_ROOT_FOLDER']
-
-    #with what
-    renderHost = ini['RENDER_HOST']
     dataRootFolder = os.path.join(prefixPath, dataRootFolder)
+
+    #Process with what?
+    rpaContainer = ini['RENDER_PYTHON_APPS_CONTAINER']
+    atmContainer = ini['AT_MODULES_CONTAINER']
+    renderHost = ini['RENDER_HOST']
 
     ribbons = ast.literal_eval(ini['RIBBONS'])
     sessions = ast.literal_eval(ini['SESSIONS'])
@@ -38,30 +40,34 @@ if __name__ == '__main__':
 
     for session in sessions:
         sessionFolders.append(os.path.join(dataRootFolder, "raw", "data", ribbons[0], session))
+
     startSection = int(ini['START_SECTION'])
     endSection   = int(ini['END_SECTION'])
 
-    #Process with what?
-    rpaContainer = ini['RENDER_PYTHON_APPS_CONTAINER']
-    atmContainer = ini['AT_MODULES_CONTAINER']
-
-    #Render info
+        #Render info
     renderProjectName = atutils.getProjectNameFromSessionFolder(sessionFolders[0])
     renderProject  = atutils.RenderProject(renderProjectOwner, renderHost, renderProjectName)
     for sessionFolder in sessionFolders:
+        print("Processing Session: " + sessionFolder)
+
         #Start with the creation of state table files
+        print("Creating statetables for session: " + sessionFolder)
         create_state_tables.run(sessionFolder, startSection, endSection, rpaContainer, prefixPath)
 
         #Create Renderstacks (multi) for the raw data
+        print("Creating ACQ Stacks for session: " + sessionFolder)
         create_rawdata_render_multi_stacks.run(sessionFolder, startSection, endSection, rpaContainer, renderProject, prefixPath)
 
         #Calculate median files
+        print("Calculating Median Files for session: " + sessionFolder)
         create_median_files.run(startSection, endSection, sessionFolder, rpaContainer, renderProject, prefixPath)
 
-        #Calculate median files
+        #Creating Flatfiled corrected data
+        print("Creating FlatField corrected data for session: " + sessionFolder)
         create_flatfield_corrected_data.run(startSection, endSection, sessionFolder, rpaContainer, renderProject, prefixPath)
 
         #Stitch the data
+        print("Stitching data for session: " + sessionFolder)
         create_stitched_sections.run(startSection, endSection, sessionFolder, atmContainer, renderProject, prefixPath)
 
     timeEnd = timeit.default_timer()
