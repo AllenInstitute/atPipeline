33ade9db621572f041bd7c955c7c58cc0fc59fcd
Totte Karlsson <tottek@gmail.com>
2018-11-27 15:20:42 -0800

    Minor update

--- atmain.py
+++ atmain.py
@@ -14,28 +14,28 @@ import timeit
 
 if __name__ == '__main__':
     timeStart = timeit.default_timer()
-    p = atutils.ATDataIni('Tottes.ini')
+    p = atutils.ATDataIni('ATData_params.ini')
 
     for sessionFolder in p.sessionFolders:
         #Start with the creation of state table files
-        print("Creating statetables for session: " + sessionFolder)
-        create_state_tables.run(p, sessionFolder)
+        #print("Creating statetables for session: " + sessionFolder)
+        #create_state_tables.run(p, sessionFolder)
 
         #Create Renderstacks (multi) for the raw data
         print("Creating ACQ Stacks for session: " + sessionFolder)
         create_rawdata_render_multi_stacks.run(p, sessionFolder)
 
         #Calculate median files
-        print("Calculating Median Files for session: " + sessionFolder)
-        create_median_files.run(p, sessionFolder)
+        #print("Calculating Median Files for session: " + sessionFolder)
+        #create_median_files.run(p, sessionFolder)
 
         #Creating Flatfiled corrected data
-        print("Creating FlatField corrected data for session: " + sessionFolder)
-        create_flatfield_corrected_data.run(p, sessionFolder)
+        #print("Creating FlatField corrected data for session: " + sessionFolder)
+        #create_flatfield_corrected_data.run(p, sessionFolder)
 
         #Stitch the data
-        print("Stitching data for session: " + sessionFolder)
-        create_stitched_sections.run(p, sessionFolder)
+        #print("Stitching data for session: " + sessionFolder)
+        #create_stitched_sections.run(p, sessionFolder)
 
     timeDuration = "{0:.2f}".format((timeit.default_timer() - timeStart)/60.0)
     print("Elapsed time: " + timeDuration + " minutes")
