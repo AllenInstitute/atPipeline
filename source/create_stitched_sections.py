b100a9ae4a283146aa91599e9b5eb39ecc3a067e
Totte Karlsson <tottek@gmail.com>
2018-11-30 12:32:47 -0800

    Updated drop_stitching_mistakes.py

--- source/create_stitched_sections.py
+++ source/create_stitched_sections.py
@@ -31,7 +31,8 @@ def run(p, sessionFolder):
 
         with open(atutils.stitchingtemplate) as json_data:
              stitching_template = json.load(json_data)
-        stitching_json    = os.path.join(stitching_dir, "flatfield""_%s_%s_%s_%d.json"%(renderProject.name, ribbon, session, sectnum))
+
+        stitching_json = os.path.join(stitching_dir, "flatfield""_%s_%s_%s_%d.json"%(renderProject.name, ribbon, session, sectnum))
         z = ribbon*100 + sectnum
 
         atutils.savestitchingjson(stitching_template, stitching_json, renderProject.owner, renderProject.name, flatfield_stack, stitched_stack, z, p.renderHost)
