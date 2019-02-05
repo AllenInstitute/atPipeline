70ea26cae14210cfbce9b9ea7b514a5ca414e29b
Totte Karlsson <tottek@gmail.com>
2019-02-05 15:59:20 -0800

    Merged in the latest

--- source/consolidate_stack_transforms.py
+++ source/consolidate_stack_transforms.py
@@ -7,7 +7,7 @@ import posixpath
 import atutils as u
 import timeit
 
-##Create 2D pointmatches for overlapping tiles
+##Create a new stack with "consolidated" transforms
 def run(p, sessionFolder):
     print ("Processing session folder: " + sessionFolder)
     [projectRoot, ribbon, session] = u.parse_session_folder(sessionFolder)
@@ -15,11 +15,8 @@ def run(p, sessionFolder):
     renderProject = u.RenderProject(p.renderProjectOwner, p.renderHost, p.projectName)
     outputFolder  = os.path.join(projectRoot, p.dataOutputFolder, "json_tilespecs_consolidation_master")
 
-    match_collection_name = "%s_HR_2D"%(renderProject.name)
-    delta = 250
-
-    cmd = "docker exec " + p.rpaContainer
-    cmd = cmd + " python -m renderapps.stitching.create_montage_pointmatches_in_place"
+    cmd = "docker exec "+ p.rpaContainer
+    cmd = cmd + " python -m rendermodules.stack.consolidate_transforms"
     cmd = cmd + " --render.host %s"                           %(renderProject.host)
     cmd = cmd + " --render.project %s"                        %(renderProject.name)
     cmd = cmd + " --render.owner %s"                          %(renderProject.owner)
@@ -27,12 +24,9 @@ def run(p, sessionFolder):
     cmd = cmd + " --render.memGB %s"                          %(p.memGB)
     cmd = cmd + " --render.port %s"                           %(p.port)
     cmd = cmd + " --pool_size %s"                             %(p.poolSize)
-    cmd = cmd + " --stack S%d_RoughAligned"             %(session)
-    cmd = cmd + " --minZ %d"                                  %(p.firstSection)
-    cmd = cmd + " --maxZ %d"                                  %(p.lastSection)
-    cmd = cmd + " --dataRoot %s"                              %(u.toDockerMountedPath(p.dataRootFolder, p.prefixPath))
-    cmd = cmd + " --matchCollection %s"                       %(match_collection_name)
-    cmd = cmd + " --delta %d"                                 %(delta)
+    cmd = cmd + " --stack S%d_RoughAligned"                        %(session)
+    cmd = cmd + " --output_stack S%d_RoughAligned_Consolidated"    %(session)
+    cmd = cmd + " --close_stack %d"%(True)
     cmd = cmd + " --output_json Test"
 
     # Run =============
---
+++ source/create_HR_tilepairs.py
@@ -0,0 +1,61 @@
+import os
+import subprocess
+import posixpath
+import atutils as u
+import timeit
+import fileinput
+from shutil import copyfile
+
+def run(p, sessionFolder):
+
+    print ("Processing session folder: " + sessionFolder)
+    [projectRoot, ribbon, session] = u.parse_session_folder(sessionFolder)
+    input_stack = "S%d_RoughAligned_Consolidated"%(session)
+
+    renderProject     = u.RenderProject(p.renderProjectOwner, p.renderHost, p.projectName)
+    jsonOutputFolder  = os.path.join(projectRoot, p.dataOutputFolder, "high_res_tilepairfiles")
+
+    # Make sure that the output folder exist
+    if os.path.isdir(jsonOutputFolder) == False:
+        os.mkdir(jsonOutputFolder)
+
+    jsonfile = os.path.join(jsonOutputFolder, "tilepairs-%d-%d-%d-nostitch.json"     %(p.zNeighborDistance, p.firstSection, p.lastSection))
+
+    #Run the TilePairClient
+    cmd = "docker exec " + p.rpaContainer
+    cmd = cmd + " java -cp /shared/render/render-ws-java-client/target/render-ws-java-client-2.0.2-SNAPSHOT-standalone.jar"
+    cmd = cmd + " org.janelia.render.client.TilePairClient"
+    cmd = cmd + " --baseDataUrl http://%s:%d/render-ws/v1"  %(p.renderHost, p.port)
+    cmd = cmd + " --owner %s"							    %(renderProject.owner)
+    cmd = cmd + " --project %s"                             %(renderProject.name)
+    cmd = cmd + " --stack %s"                               %(input_stack)
+    cmd = cmd + " --minZ %d"                                %(p.firstSection)
+    cmd = cmd + " --maxZ %d"                                %(p.lastSection)
+    cmd = cmd + " --toJson %s"                              %(u.toDockerMountedPath(jsonfile, p.prefixPath))
+    cmd = cmd + " --excludeCornerNeighbors %s"              %(p.excludeCornerNeighbors)
+    cmd = cmd + " --excludeSameSectionNeighbors %s"         %(p.excludeSameSectionNeighbors)
+    cmd = cmd + " --zNeighborDistance %s"                   %(p.zNeighborDistance)
+    cmd = cmd + " --xyNeighborFactor %s"                    %(p.xyNeighborFactor)
+
+    #Run =============
+    print ("Running: " + cmd.replace('--', '\n--'))
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in proc.stdout.readlines():
+        print (line)
+
+    #Prepare json file for the SIFTPointMatch Client
+    jsonfileedit      = os.path.join(jsonOutputFolder, "tilepairs-%d-%d-%d-nostitch-EDIT.json"%(p.zNeighborDistance, p.firstSection, p.lastSection))
+    copyfile(jsonfile, jsonfileedit)
+    for line in fileinput.input(jsonfileedit, inplace=True):
+      print (line.replace("render-parameters", "render-parameters?removeAllOption=true"), end="")
+
+if __name__ == "__main__":
+    timeStart = timeit.default_timer()
+    f = os.path.join('..', 'ATData.ini')
+    p = u.ATDataIni(f)
+
+    for sessionFolder in p.sessionFolders:
+        run(p, sessionFolder)
+
+    timeDuration = "{0:.2f}".format((timeit.default_timer() - timeStart)/60.0)
+    print("Elapsed time: " + timeDuration + " minutes")
