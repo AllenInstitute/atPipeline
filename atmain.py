721aceaea2aa44013282d2c089e1b8f88fe4de90
TotteKarlsson <totte@dunescientific.com>
2019-02-11 18:00:35 -0800

    Individual pipeline modules now require a valid ini file passed as an argument Moved inifile validation code to atutils

--- atmain.py
+++ atmain.py
@@ -1,123 +1,118 @@
 import os
 import logging
-import sys
-import shutil
-import source.atutils as u
-from source import *
 import timeit
-import argparse
+import pathlib
+import docker
+from source import *
+import source.atutils as u
 
 def main():
+    logger = logging.getLogger("atPipeline")
+    logger.setLevel(logging.DEBUG)
+    timeStart = timeit.default_timer()
+
     try:
-        print ("Hello")
-        logger = logging.getLogger("atPipeline")
-        logger.setLevel(logging.DEBUG)
-        timeStart = timeit.default_timer()
-        parser = argparse.ArgumentParser()
-        parser.add_argument('parameter_file', help='Input file')
-        args = parser.parse_args()
-        parameterFile = args.parameter_file
+        #Get processing parameters
+        parameters =u.validateATCoreInputAndOutput()
 
-        #Check that input file exists, if not bail
-        if os.path.isfile(parameterFile) == False:
-            raise ValueError("The input file: " + parameterFile + " don't exist. Bailing..")
+        dockerClient = docker.from_env()
+        atcore = dockerClient.containers.get("atcore")
+        render = dockerClient.containers.get("init_render_1")
 
-        p = u.ATDataIni(parameterFile)
+        if render.status != "running":
+            raise ValueError("The Render docker container is not running!")
 
-        #Copy parameter file to root of processed data output folder
-        outFolder = os.path.join(p.dataRootFolder, p.dataOutputFolder)
-        if os.path.isdir(outFolder) == False:
-            os.mkdir(outFolder)
+        if atcore.status != "running":
+            raise ValueError("The atcore docker container is not running!")
 
-        shutil.copy2(parameterFile, outFolder)
-
-        for sessionFolder in p.sessionFolders:
+        for sessionFolder in parameters.sessionFolders:
             #Start with the creation of state table files
-            if p.createStateTables == True:
+            if parameters.createStateTables == True:
                print("Creating statetables for session: " + sessionFolder)
-               create_state_tables.run(p, sessionFolder)
+               create_state_tables.run(parameters, sessionFolder)
 
             #Create Renderstacks (multi) for the raw data
-            if p.createRawDataRenderMultiStacks == True:
+            if parameters.createRawDataRenderMultiStacks == True:
                print("Creating Raw Data Stacks for session: " + sessionFolder)
-               create_rawdata_render_multi_stacks.run(p, sessionFolder)
+               create_rawdata_render_multi_stacks.run(parameters, sessionFolder)
 
             #Calculate median files
-            if p.createMedianFiles == True:
+            if parameters.createMedianFiles == True:
                print("Calculating Median Files for session: " + sessionFolder)
-               create_median_files.run(p, sessionFolder)
+               create_median_files.run(parameters, sessionFolder)
 
             #Creating Flatfiled corrected data
-            if p.createFlatFieldCorrectedData == True:
+            if parameters.createFlatFieldCorrectedData == True:
                print("Creating FlatField corrected data for session: " + sessionFolder)
-               create_flatfield_corrected_data.run(p, sessionFolder)
+               create_flatfield_corrected_data.run(parameters, sessionFolder)
 
             #Stitch the data
-            if p.createStitchedSections == True:
+            if parameters.createStitchedSections == True:
                print("Stitching data for session: " + sessionFolder)
-               create_stitched_sections.run(p, sessionFolder)
+               create_stitched_sections.run(parameters, sessionFolder)
 
             #Drop stitching mistakes
-            if p.dropStitchingMistakes == True:
+            if parameters.dropStitchingMistakes == True:
                print("Stitching data for session: " + sessionFolder)
-               drop_stitching_mistakes.run(p, sessionFolder)
+               drop_stitching_mistakes.run(parameters, sessionFolder)
 
             #Create lowres stacks
-            if p.createLowResStacks == True:
+            if parameters.createLowResStacks == True:
                print("Create Lowres Stacks for Session: " + sessionFolder)
-               create_lowres_stacks.run(p, sessionFolder)
+               create_lowres_stacks.run(parameters, sessionFolder)
 
             #Create TilePairs
-            if p.createLRTilePairs == True:
+            if parameters.createLRTilePairs == True:
                print("Create TilePairs for Session:" + sessionFolder)
-               create_LR_tilepairs.run(p, sessionFolder)
+               create_LR_tilepairs.run(parameters, sessionFolder)
 
             #Create PointMatches
-            if p.createLRPointMatches == True:
+            if parameters.createLRPointMatches == True:
                print("Create Pointmatches: "+ sessionFolder)
-               create_LR_pointmatches.run(p, sessionFolder)
+               create_LR_pointmatches.run(parameters, sessionFolder)
 
             #Rough Align
-            if p.createRoughAlignedStacks == True:
+            if parameters.createRoughAlignedStacks == True:
                print("Create Rough Aligned Stacks: "+ sessionFolder)
-               create_rough_aligned_stacks.run(p, sessionFolder)
+               create_rough_aligned_stacks.run(parameters, sessionFolder)
 
             #Apply lowres transforms to highres stacks
-            if p.applyLowResToHighRes == True:
+            if parameters.applyLowResToHighRes == True:
                print("Applying low resolution alignment transforms to high resoloution stack: "+ sessionFolder)
-               apply_lowres_to_highres.run(p, sessionFolder)
+               apply_lowres_to_highres.run(parameters, sessionFolder)
 
             #Consolidate transforms
-            if p.consolidateRoughAlignedStackTransforms == True:
+            if parameters.consolidateRoughAlignedStackTransforms == True:
                print("Consolidating Rough Aligned RenderTransforms: "+ sessionFolder)
-               consolidate_stack_transforms.run(p, sessionFolder)
+               consolidate_stack_transforms.run(parameters, sessionFolder)
 
             #Create 2D point matches
-            if p.create2DPointMatches == True:
+            if parameters.create2DPointMatches == True:
                print("Create 2D pointmatches (tile on tile) for session: "+ sessionFolder)
-               create_2D_pointmatches.run(p, sessionFolder)
+               create_2D_pointmatches.run(parameters, sessionFolder)
 
            #Create HR tile pairs
-            if p.createHRTilePairs == True:
+            if parameters.createHRTilePairs == True:
                print("Create HighResolution tile pairs.")
-               create_HR_tilepairs.run(p, sessionFolder)
+               create_HR_tilepairs.run(parameters, sessionFolder)
 
             #Create HR pointmatches
-            if p.createHRPointMatches == True:
+            if parameters.createHRPointMatches == True:
                print("Create HighResolution point matches.")
-               create_HR_pointmatches.run(p, sessionFolder)
+               create_HR_pointmatches.run(parameters, sessionFolder)
 
             #Create FineAligned stack
-            if p.createFineAlignedStacks == True:
+            if parameters.createFineAlignedStacks == True:
                print("Create FineAligned Stacks.")
-               create_fine_aligned_stacks.run(p, sessionFolder)
+               create_fine_aligned_stacks.run(parameters, sessionFolder)
 
 
         timeDuration = "{0:.2f}".format((timeit.default_timer() - timeStart)/60.0)
         print("Elapsed time: " + timeDuration + " minutes")
 
     except ValueError as error:
-        logger.error(error)
+        print ("An error occured: " + str(error))
+
 
 if __name__ == '__main__':
     main()
