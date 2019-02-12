1bf594e011644b520ebd674dda0772ceb4b7ec1f
TotteKarlsson <totte@dunescientific.com>
2019-02-11 18:03:56 -0800

    Line endings changes?

--- docker/start-render-services.sh
+++ docker/start-render-services.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-if [[ $(uname -s) == CYGWIN* ]];then
-    docker-compose -f ./init/docker-compose-windows.yml up -d
-else 
-    docker-compose -f ./init/docker-compose-mac.yml up -d
-fi
+#if [[ $(uname -s) == CYGWIN* ]];then
+    docker-compose -f ./init/docker-compose-wsl.yml up -d
+#else 
+#    docker-compose -f ./init/docker-compose-mac.yml up -d
+#fi
