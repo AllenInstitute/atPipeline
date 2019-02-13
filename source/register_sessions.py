8d58d23f91ca6e1588f3e990f2f4b87e66a54fcf
Totte Karlsson <tottek@gmail.com>
2019-02-13 13:55:18 -0800

    More registration

--- source/register_sessions.py
+++ source/register_sessions.py
@@ -1,50 +1,6 @@
 import atutils as u
 import os
-
-##def parse_project_directory(line):
-##	proj = line.split("raw")
-##	projectdirectory = proj[0]
-##
-##	tok = line.split("/")
-##	ribbondir = tok[len(tok)-2]
-##	sessiondir = tok[len(tok)-1]
-##	ribbon = int(ribbondir[6:])
-##	session = int(sessiondir[7:])
-##
-##	return [projectdirectory, ribbon, session]
-##
-##def get_channel_names(directory):
-##	#return os.listdir(directory)
-##	directory_list = list()
-##	for root, dirs, files in os.walk(directory, topdown=False):
-##	   for name in dirs:
-##  		    directory_list.append(os.path.join(root, name))
-##	return dirs
-##
-##def parseprojectroot(projectdirectory):
-##    #print projectdirectory
-##    tok = projectdirectory.split("/")
-##    dataind = tok.index('data')
-##    return tok[dataind+1]
-
-def saveregistrationjson(reg,registrationfile,owner,project,stack,referenceStack,outputStack,section):
-	reg['owner'] = owner
-	reg['project'] = project
-	reg['stack'] = stack
-	reg['referencestack']=referenceStack
-	reg['outputStack'] = outputStack
-	reg['section'] = section
-	reg['steps'] =  5
-	reg['maxEpsilon'] = 2
-	reg['minOctaveSize'] = 1000
-	reg['maxOctaveSize'] = 2000
-	reg['initialSigma'] = 2.5
-	reg['percentSaturated'] = 0.9
-	reg['contrastEnhance'] = False
-
-	with open(registrationfile,'w') as outfile:
-		json.dump(reg,outfile,indent=4)
-
+import json
 
 def run(p, sessionFolder):
     owner = "multchan"
@@ -53,47 +9,44 @@ def run(p, sessionFolder):
     print ("Processing session folder: " + sessionFolder)
     [projectRoot, ribbon, session] = u.parse_session_folder(sessionFolder)
 
-    ##This file seem to contain session folders, e.g.
-    ##/nas5/data/M367240B_SSTPV_LaminB1_smallvol/raw/data/Ribbon0011/session01
-##    with open("/pipeline/forcron/registration/confirm_data2process2") as f:
-##        alldirnames = f.readlines()
+    renderProject     = u.RenderProject(p.renderProjectOwner, p.renderHost, p.projectName)
+    jsonOutputFolder  = os.path.join(projectRoot, p.dataOutputFolder, "registration")
 
-
-    channels = u.getChannelNamesInSessionFolder(sessionFolder)
-
-    z = ribbon*100+sectnum
-
-    #create file that consists of celery job commands
-    filename = "log/runme_sect_%s_%d_%d_%s.sh"%(project, ribbon,session,sectnum)
-    f = open(filename,'w')
-
-    registrationfile = "%s/log/registration_%s_%s_%s_%d.json"%(curdir,project,ribbon,session,sectnum)
+    # Make sure that the output folder exist
+    if os.path.isdir(jsonOutputFolder) == False:
+        os.mkdir(jsonOutputFolder)
 
     #stacks
-    reference_stack     = "STI_FF_Session1"
-    stitched_stack      = "STI_FF_Session%d"%(int(session))
-    registered_stack    = "1_REG_FF_S0%d"%(int(session))
+    reference_stack     = "S1_Stitched"
+    stitched_stack      = "S%d_Stitched"%(int(session))
+    outputStack         = "S%d_Registered"%(int(session))
 
-    #directories
-    flatfield_dir = "%s/processed/Flatfield/"%projectroot
-    deconv_dir = "%s/processed/Deconv/"%projectroot
+    #Loop over sections?
+    sectnum = 1
 
-    #create files
-    with open(registrationtemplate) as json_data:
-   	    reg = json.load(json_data)
+    #This is the registration input (json) file
+    inputJSON = os.path.join(jsonOutputFolder, "registration_%s_%s_%d.json"%(ribbon, session, sectnum))
 
-    saveregistrationjson(reg,registrationfile,owner,project,stitched_stack,reference_stack,registered_stack,z)
+    #create input file for registration
+    with open(u.registrationTemplate) as json_data:
+        t = json.load(json_data)
+
+    z = ribbon*100+sectnum
+    u.saveRegistrationJSON(t, inputJSON, p.renderHost, renderProject.owner, renderProject.name, stitched_stack, reference_stack, outputStack, z)
 
     #run
     if session > 1:
-   	    print ("found DAPI file")
-   	    cmd5 = "java -cp /pipeline/sharmi/Sharmi_tools/atmodules-branches/fix_bug_phasecorr/at_modules/target/allen-1.0-SNAPSHOT-jar-with-dependencies.jar at_modules.Register --input_json %s"%registrationfile
-   	    os.system(cmd5)
-    else:
-    	cmd5 = ""
+        cmd = "java -cp /pipeline/sharmi/Sharmi_tools/atmodules-branches/fix_bug_phasecorr/at_modules/target/allen-1.0-SNAPSHOT-jar-with-dependencies.jar"
+        cmd = cmd + " at_modules.Register"
+        cmd = cmd + " --input_json %s"%inputJSON
 
+        #Run =============
+        print ("Running: " + cmd.replace('--', '\n--'))
 
-    f.close()
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in proc.stdout.readlines():
+            print (line)
+
 
 if __name__ == "__main__":
     #This script need a valid INI file to be passed as an argument
