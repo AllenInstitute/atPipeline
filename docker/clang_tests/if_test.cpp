3a9597a42fd57a10a258419c6195771ee3cc4ca0
Totte Karlsson <tottek@gmail.com>
2019-03-14 11:58:36 -0700

    Adding a clang container

---
+++ docker/clang_tests/if_test.cpp
@@ -0,0 +1,13 @@
+//if block with init-statement, the example is a bit silly,
+// but you can use it to check if your compiler supports C++17:
+#include <iostream>
+
+int main() {
+    // if block with init-statement:
+    if(int a = 5; a < 8) {
+        std::cout << "Local variable a is < 8\n";
+    } else {
+        std::cout << "Local variable a is >= 8\n";
+    }
+    return 0;
+}
