deb49c4bbb46712d1d55ab992a17de73f464b495
Totte Karlsson <tottek@gmail.com>
2019-03-14 19:09:44 -0700

    Getting a binary to parse atdata with

---
+++ docker/start-clang.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+image_tag="clang_image"
+container_name="clang"
+export DISPLAY=10.128.26.56:0.0
+
+echo " ======== Starting Docker container with name: $container_name =============="
+
+echo "Killing container with name: $container_name"
+docker kill $container_name
+docker rm $container_name 
+
+echo "Starting container with name: $container_name"
+docker run -e DISPLAY=$DISPLAY -d --name $container_name \
+-v c:\\pDisk\\ATPipeline\\docker\\third-party-libs:/libs  \
+-v c:\\pDisk\\ATPipeline\\docker\\third-party-libs\\builds:/builds  \
+-it $image_tag /bin/bash
+
+echo "Done.."
