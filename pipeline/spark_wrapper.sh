986e2676956ad8c64e2c750ebfa0e614e87c8b56
Eric Perlman <perlman@gmail.com>
2019-06-18 13:35:05 -0700

    Unique password file per user.

--- pipeline/spark_wrapper.sh
+++ pipeline/spark_wrapper.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 #
-# A wrapper to spark-submit to make sure the current userid can resolve
-# Credit to:
+# A wrapper to spark-submit to make sure the current userid can resolve.
+# Based on:
 # https://stackoverflow.com/questions/45198252/apache-spark-standalone-for-anonymous-uid-without-user-name
 #
 
-cat /etc/passwd > /tmp/passwd
-echo "$(id -u):x:$(id -u):$(id -g):dynamic uid:$SPARK_HOME:/bin/false" >> /tmp/passwd
+cat /etc/passwd > /tmp/passwd.$(id -u)
+echo "$(id -u):x:$(id -u):$(id -g):dynamic uid:$SPARK_HOME:/bin/false" >> /tmp/passwd.$(id -u)
 
-export NSS_WRAPPER_PASSWD=/tmp/passwd
-# NSS_WRAPPER_GROUP must be set for NSS_WRAPPER_PASSWD to be used
+export NSS_WRAPPER_PASSWD=/tmp/passwd.$(id -u)
 export NSS_WRAPPER_GROUP=/etc/group
-
 export LD_PRELOAD=libnss_wrapper.so
 
 exec "$@"
