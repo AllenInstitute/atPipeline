bfd15607f9ef494aa4f86448dae6f4140fedb56e
Totte Karlsson <tottek@gmail.com>
2019-06-20 13:50:57 -0700

    Ran all tests succesfully Curiosuly, deleteing the data for the M33Quarter still fails for some reason. Needs some investigation.

--- atpipeline/at_test_utils.py
+++ atpipeline/at_test_utils.py
@@ -1,10 +1,23 @@
 import os
 
 def compare_file_in_folders(the_file, folder_1,folder_2):
-    f1 = open(os.path.join(folder_1, the_file)).read()
-    f2 = open(os.path.join(folder_2, the_file)).read()
+    print("Checking file: " + the_file)
+    print("Folder 1: " + folder_1)
+    print("Folder 2: " + folder_2)
+ 
+    try:
+        f1 = open(os.path.join(folder_1, the_file)).read()
+    except IOError:
+        print ("Failed opening/reading the file: " + os.path.join(folder_1, the_file))
+        return False
 
-    diff_result = (f1 == f2)
+    try:
+        f2 = open(os.path.join(folder_2, the_file)).read()
+    except IOError:
+        print ("Failed opening/reading the file: " + os.path.join(folder_2, the_file))
+        return False
 
-    print (diff_result)
+    diff_result = (f1.strip()== f2.strip())
+
+    print ("Diff result: " + str(diff_result))
     return diff_result
